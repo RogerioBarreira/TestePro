250f947c24ae282d55826ac077508dca93b469e1
Rogerio Barreira <shsquads.rbarreira@terceiros.mag.com.br>
2023-12-18 23:12:12 -0300

    TableView
    Criaçao

--- TestePro/TestePro/Controllers/List/View/ListView.swift
+++ TestePro/TestePro/Controllers/List/View/ListView.swift
@@ -6,3 +6,47 @@
 //
 
 import Foundation
+import UIKit
+
+class ListView: UIView {
+    
+    let myTableView: UITableView = {
+        let tableView = UITableView()
+        tableView.translatesAutoresizingMaskIntoConstraints = false
+        tableView.register(ListCell.self, forCellReuseIdentifier: ListCell.identifier)
+        tableView.backgroundColor = .clear
+        return tableView
+    }()
+    
+    override init(frame: CGRect) {
+        super.init(frame: frame)
+        addElementsVisual()
+        configConstraints()
+    }
+    
+    required init?(coder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+    
+    func addElementsVisual() {
+        addSubview(myTableView)
+    }
+    
+    func configConstraints() {
+        NSLayoutConstraint.activate([
+            myTableView.topAnchor.constraint(equalTo: self.safeAreaLayoutGuide.topAnchor),
+            myTableView.leadingAnchor.constraint(equalTo: self.safeAreaLayoutGuide.leadingAnchor),
+            myTableView.trailingAnchor.constraint(equalTo: self.safeAreaLayoutGuide.trailingAnchor),
+            myTableView.bottomAnchor.constraint(equalTo: self.safeAreaLayoutGuide.bottomAnchor)
+        ])
+    }
+    
+    func setupTableView(delegate: UITableViewDelegate, dataSource: UITableViewDataSource) {
+        myTableView.delegate = delegate
+        myTableView.dataSource = dataSource
+    }
+    
+    func tableViewReloadData() {
+        myTableView.reloadData()
+    }
+}
--- TestePro/TestePro/Controllers/List/View/ListViewController.swift
+++ TestePro/TestePro/Controllers/List/View/ListViewController.swift
@@ -9,11 +9,44 @@ import UIKit
 
 class ListViewController: UIViewController {
 
+    lazy var viewList: ListView = {
+        let view = ListView()
+        return view
+    }()
+    
+    override func loadView() {
+        super.loadView()
+        self.view = viewList
+    }
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         view.backgroundColor = .red
+        viewList.setupTableView(delegate: self, dataSource: self)
     }
 
 
+}
+
+extension ListViewController: UITableViewDelegate, UITableViewDataSource {
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return 20
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        if let cell = tableView.dequeueReusableCell(withIdentifier: ListCell.identifier, for: indexPath) as? ListCell {
+            cell.setupCell(name: "Rogerio")
+            return cell
+        }
+       return UITableViewCell()
+        
+        
+    }
+    
+    func tableView(_ tableView: UITableView, heightForRowAt indexPath: IndexPath) -> CGFloat {
+        return 50
+    }
+    
+    
 }
 
